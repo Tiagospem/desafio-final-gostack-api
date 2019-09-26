19375b8b05ef062008f3833a0ce74e1deed34fe9
Tiagospem <tiagospem@gmail.com>
2019-09-26 13:54:19 -0300

    added Yup to validate inputs

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,13 +1,68 @@
+import * as Yup from 'yup'
 import User from '../models/User'
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body)
-    return res.json(user)
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(8)
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' })
+    }
+
+    const userExists = await User.findOne({ where: { email: req.body.email } })
+
+    if (userExists) {
+      return res.status(400).json({ erro: 'User already exists' })
+    }
+
+    const { id, name, email } = await User.create(req.body)
+
+    return res.json({ id, name, email })
   }
 
   async update(req, res) {
-    return res.json({ ok: true })
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(8),
+      password: Yup.string()
+        .min(8)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      passwordConfirm: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      )
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' })
+    }
+    const { email, oldPassword } = req.body
+    const user = await User.findByPk(req.userId)
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } })
+      if (userExists) {
+        return res.status(400).json({ erro: 'The email already exists' })
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ erro: 'Password does not match' })
+    }
+
+    const { id, name } = await user.update(req.body)
+
+    return res.json({ id, name, email })
   }
 }
 export default new UserController()
