4b77a07923cb2c88a348f570caa74b315911bc3c
Tiagospem <tiagospem@gmail.com>
2019-09-27 16:51:02 -0300

    added nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7379e174064968',
+    pass: '760c5d8e28deca'
+  },
+  default: {
+    from: 'Meetup App <noreply@meetupapp.io>'
+  }
+}
